e3c9941cd99925545eba7bd900802b1fdeb3fe1b
Ashna Shah-Grover <as19@williams.edu>
2019-08-26 17:17:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", change)
+
+function change() {
+  console.log()
+  let text = document.querySelector("#text")
+  text.innerHTML = "This is really cool!"
+}
